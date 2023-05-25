a5ec8e22bafff525bcfa1ebeb0f69253e0d7f332
rajkumer060301 <raj.kumar_cs19@gla.ac.in>
2023-05-25 16:14:50 +0530

    punch out part completed

--- admin/todowork.php
+++ admin/todowork.php
@@ -1,12 +1,31 @@
 <?php
 session_start();
-echo $id =  $_SESSION['teamID']."<br>";
-echo $punchout = $_POST['punchout']."<br>";
-echo $lunchin = $_POST['lunchin']."<br>";
-echo $lunchout = $_POST['lunchout']."<br>";
-echo $task = $_POST['task']."<br>";
-echo $_POST['upwork']."<br>";
-echo $_POST['webtracker'];
+$connection = mysqli_connect('localhost','root','','firstday');
+if(!$connection){
+     echo "<script>alert('Database not connected')</script>";
+    //  echo "<script>location.href='addteammate.php'</script>";
+} 
+$iD =  $_SESSION['teamID'];
+$punchout = $_POST['punchout'];
+// echo $lunchin = $_POST['lunchin']."<br>";
+// echo $lunchout = $_POST['lunchout']."<br>";
+$task = $_POST['task'];
+$upwork = $_POST['upwork'];
+$webtracker = $_POST['webtracker'];
 
+$update = "UPDATE todowork SET `punchout`='$punchout' ,`task`='$task',`upwork`='$upwork',`webtracker`='$webtracker' WHERE id=".$iD;
+
+if(mysqli_query($connection,$update)){
+    echo "<script>alert('Today task sumbmitted succussfully')</script>";
+    echo "<script>location.href='memberprofile.php'</script>";
+    // echo "Updation successfully";
+
+}
+else{
+    // echo "<script>alert('Task not addred')</script>";
+    // echo "<script>location.href='memberprofile.php'</script>";
+    // echo "data not Updated";
+
+}
 
 ?>
