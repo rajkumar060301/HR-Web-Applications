08cb3692c7e8bb8f2f9786d874442aa65df0a044
rajkumer060301 <raj.kumar_cs19@gla.ac.in>
2023-05-25 16:25:06 +0530

    commit

--- admin/index3.php
+++ admin/index3.php
@@ -41,15 +41,7 @@
 
   </style>
 </head>
-<!--
-`body` tag options:
 
-  Apply one or more of the following classes to to the body tag
-  to get the desired effect
-
-  * sidebar-collapse
-  * sidebar-mini
--->
 
 <body class="hold-transition sidebar-mini">
 <!-- <?php
