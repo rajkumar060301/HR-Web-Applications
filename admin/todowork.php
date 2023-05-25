b3e46fa7f64fde08ecfcae577dc8149925d72523
rajkumer060301 <raj.kumar_cs19@gla.ac.in>
2023-05-25 16:16:11 +0530

    comment is deleted

--- admin/todowork.php
+++ admin/todowork.php
@@ -3,12 +3,10 @@ session_start();
 $connection = mysqli_connect('localhost','root','','firstday');
 if(!$connection){
      echo "<script>alert('Database not connected')</script>";
-    //  echo "<script>location.href='addteammate.php'</script>";
 } 
 $iD =  $_SESSION['teamID'];
 $punchout = $_POST['punchout'];
-// echo $lunchin = $_POST['lunchin']."<br>";
-// echo $lunchout = $_POST['lunchout']."<br>";
+
 $task = $_POST['task'];
 $upwork = $_POST['upwork'];
 $webtracker = $_POST['webtracker'];
@@ -18,13 +16,11 @@ $update = "UPDATE todowork SET `punchout`='$punchout' ,`task`='$task',`upwork`='
 if(mysqli_query($connection,$update)){
     echo "<script>alert('Today task sumbmitted succussfully')</script>";
     echo "<script>location.href='memberprofile.php'</script>";
-    // echo "Updation successfully";
 
 }
 else{
-    // echo "<script>alert('Task not addred')</script>";
-    // echo "<script>location.href='memberprofile.php'</script>";
-    // echo "data not Updated";
+    echo "<script>alert('Task not addred')</script>";
+    echo "<script>location.href='memberprofile.php'</script>";
 
 }
 
