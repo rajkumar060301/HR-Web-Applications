1bc68baf96d726367e14973e611d0c85fd566feb
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-05-26 17:36:11 +0530

    lunch in commit

--- admin/lunchin.php
+++ admin/lunchin.php
@@ -10,35 +10,33 @@ if(!$myConnection){
 $iD=$_SESSION['teamID'];
 date_default_timezone_set("Asia/Calcutta");
 $time = date('h:i:s');
-$read_query = "SELECT * FROM todowork where id=".$iD;
-
-$data = mysqli_query($myConnection, $read_query);
-
-if(mysqli_num_rows($data)>0){
-
-    $row = mysqli_fetch_array($data);
-
-       
-} else {
-   echo "Record Not found";
-}
-
-$punchin = $row['punchin'];
-
-
 // before fetch date and id of team member from todowork table
-$currentDate = date("Y/m/d"); 
+$currentDate = date("Y/m/d");
 $lunchin = '00:00:00';
-$update = "UPDATE todowork SET `lunchin`='$time'  WHERE `id`='$iD' AND `date`='$currentDate' AND `punchin` = '$punchin' AND `lunchin` ='$lunchin' ";
+$read_todowork = " SELECT * FROM todowork where `id`='$iD' AND `date`='$currentDate' AND $lunchin ";
 
-if(mysqli_query($myConnection,$update)){
+$data_todo = mysqli_query($myConnection, $read_todowork);
+
+if(mysqli_num_rows($data_todo)>0){
+
+  $row_data = mysqli_fetch_array($data_todo);
+
+  echo "<script>alert('Already lunch in today')</script>";
+  echo "<script>location.href='memberprofile.php'</script>";
+
+
+} else {
+
+  $update = "UPDATE todowork SET `lunchin`='$time'  WHERE `id`='$iD' AND `date`='$currentDate' ";
+
+  if (mysqli_query($myConnection, $update)) {
     echo "<script>alert('Lunchin is successfully')</script>";
     echo "<script>location.href='memberprofile.php'</script>";
 
-}
-else{
+  } else {
     echo "<script>alert('Lunch in is not successfully')</script>";
     echo "<script>location.href='memberprofile.php'</script>";
 
+  }
 }
 ?>
